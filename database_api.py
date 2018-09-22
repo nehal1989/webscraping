5bb66226840b6d0026e65dde636ac82dba0a3b6b
Nehal <nehal26@gmail.com>
2018-09-22 14:41:14 +0100

    Added database

--- database_api.py
+++ database_api.py
@@ -14,12 +14,21 @@ def initialiase_database(database_name='sysk'):
     except sqlite3.OperationalError:
         print(f'Looks like the table already exist, no worries, carrying on')
 
-
-    db.commit() # comitting changes to the DB
-
-
+    db.commit()  # comitting changes to the DB
     db.close()
 
+def add_data(podcast_data):
+    db = sqlite3.connect(f'/Users/Nehal/Programming/python_projects/webscraping/sysk.db')
+    cursor = db.cursor()
+
+    try:
+        cursor.executemany(''' INSERT INTO podcastInfo(title, date) VALUES(?,?)''', podcast_data)
+
+    except sqlite3.OperationalError:
+        print(f'Looks like the table already exist, no worries, carrying on')
+
+    db.commit()  # comitting changes to the DB
+    db.close()
 
 
 
--- scraper.py
+++ scraper.py
@@ -1,5 +1,6 @@
 import requests
 import bs4
+from collections import namedtuple
 
 URL = "https://www.stuffyoushouldknow.com/podcasts/sysk-archive.htm"
 
@@ -9,21 +10,23 @@ def pull_website(URL):
     return site
 
 def scrape_site(site):
-
     soup = bs4.BeautifulSoup(site.text, 'html.parser')
     podcast_list_soup = soup.select('.h4')
     podcast_date_soup = soup.select('.separation')
 
-    podcast_list = [tags.get_text().strip() for tags in podcast_list_soup]
+
+    podcast = namedtuple('podcast', 'title date')
+
+    podcast_titles = [tags.get_text().strip() for tags in podcast_list_soup]
     podcast_date = [tags.get_text().strip() for tags in podcast_date_soup]
 
-    new_list = [f"Podcast Title: {title}, {date}" for title, date
-                in zip(podcast_list, podcast_date)]
+    podcast_info = [podcast(title, date) for title, date
+                in zip(podcast_titles, podcast_date)]
 
-    for idx, txt in enumerate(new_list,1):
-        print(f"{idx}. {txt}")
+    for idx, details in enumerate(podcast_info,1):
+        print(f"{idx}. {details.title}, {details.date}")
 
-    return new_list
+    return podcast_info
 
 if __name__ == '__main__':
     site = pull_website(URL)
